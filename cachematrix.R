0055ce195c88f4d3007c478498e2fae45c90c578
Gul Muneer <gulmuneerbaloch@gmail.com>
2021-04-18 17:04:06 +0800

    Initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,54 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+library(MASS)
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  inv <- NULL                 #initialize inverse as NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x          #function to get matrix x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() {inv}
+                      #function to opbtain inverse of the matrix
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
+#This is used to get the cache data
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) #gets cache data
+  {
+  inv <- x$getinv()
+  if(!is.null(inv)){          #checking whenther inverse is NULL
+    message("getting cached data!")
+    return(inv)               #returns inverse value
+  }
+  data <- x$get()
+  inv <- solve(data,...)      #calculates inverse value
+  x$setinv(inv)
+  inv                         ## Return a matrix that is the inverse of 'x'
 }
+
+##Sample run 1:
+f <- makeCacheMatrix(matrix(1:4, 2, 2))
+f$get()
+f$getinv()  
+cacheSolve(f)
+
+## Sample run 2:
+
+x = rbind(c(1, -1/4), c(-1/4, 1))
+m = makeCacheMatrix(x)
+
+m$get()
+cacheSolve(m)
+cacheSolve(m)
+
+
+
